16309638d637913d37e2121e5d274b41e424e0c5
최원석 <cws4987@siwonschool.com>
2023-03-03 10:07:20 +0900

    merge

--- src/modules/index.js
+++ src/modules/index.js
@@ -4,30 +4,20 @@ import auth, { authSaga } from './auth';
 import loading from './loading';
 import user, { userSaga } from './user';
 import write, { writeSaga } from './write';
-<<<<<<< HEAD
-=======
 import post, { postSaga } from './post';
 import posts, { postsSaga } from './posts';
->>>>>>> 57f614e03d7162c21e011550030e6bdc6a864b49
 
 const rootReducer = combineReducers({
   auth,
   loading,
   user,
   write,
-<<<<<<< HEAD
-});
-
-export function* rootSaga() {
-  yield all([authSaga(), userSaga(), writeSaga()]);
-=======
   post,
   posts,
 });
 
 export function* rootSaga() {
   yield all([authSaga(), userSaga(), writeSaga(), postSaga(), postsSaga()]);
->>>>>>> 57f614e03d7162c21e011550030e6bdc6a864b49
 }
 
 export default rootReducer;
